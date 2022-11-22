0ed47f3c3d64e36bf2ef67949cf7d5a79fb112d7
Angel de Caso <AngelCastleCraft@gmail.com>
2022-11-22 12:55:51 +0100

    Sensores funcionan ^^

--- app/src/main/java/com/example/planta/MainActivity.java
+++ app/src/main/java/com/example/planta/MainActivity.java
@@ -3,17 +3,24 @@ package com.example.planta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_planta);
-
+        setContentView(R.layout.activity_main);
+        Button boton = (Button) findViewById(R.id.buton_empezar);
         //ejemplo
         // hola, funciona?
 
+    }
+    public  void empezar(View v){
+        Intent intent = new Intent(getApplicationContext(),ActivityPlanta.class);
+        startActivity(intent);
     }
 }
